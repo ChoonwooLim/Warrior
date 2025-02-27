20a047aed3509f7b993ed8f0d64361c6ef7e0cce
Steven Lim <choonwoo49@gmai.com>
2025-02-27 09:44:02 +0900

    Add project files.

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/Warrior/UHT/WarriorCharacter.generated.h
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "WarriorCharacter.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef WARRIOR_WarriorCharacter_generated_h
+#error "WarriorCharacter.generated.h already included, missing '#pragma once' in WarriorCharacter.h"
+#endif
+#define WARRIOR_WarriorCharacter_generated_h
+
+#define FID_Warrior_Source_Warrior_WarriorCharacter_h_21_SPARSE_DATA
+#define FID_Warrior_Source_Warrior_WarriorCharacter_h_21_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Warrior_Source_Warrior_WarriorCharacter_h_21_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Warrior_Source_Warrior_WarriorCharacter_h_21_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Warrior_Source_Warrior_WarriorCharacter_h_21_ACCESSORS
+#define FID_Warrior_Source_Warrior_WarriorCharacter_h_21_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAWarriorCharacter(); \
+	friend struct Z_Construct_UClass_AWarriorCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AWarriorCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Warrior"), NO_API) \
+	DECLARE_SERIALIZER(AWarriorCharacter)
+
+
+#define FID_Warrior_Source_Warrior_WarriorCharacter_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AWarriorCharacter(AWarriorCharacter&&); \
+	NO_API AWarriorCharacter(const AWarriorCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AWarriorCharacter); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AWarriorCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AWarriorCharacter) \
+	NO_API virtual ~AWarriorCharacter();
+
+
+#define FID_Warrior_Source_Warrior_WarriorCharacter_h_18_PROLOG
+#define FID_Warrior_Source_Warrior_WarriorCharacter_h_21_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Warrior_Source_Warrior_WarriorCharacter_h_21_SPARSE_DATA \
+	FID_Warrior_Source_Warrior_WarriorCharacter_h_21_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Warrior_Source_Warrior_WarriorCharacter_h_21_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Warrior_Source_Warrior_WarriorCharacter_h_21_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Warrior_Source_Warrior_WarriorCharacter_h_21_ACCESSORS \
+	FID_Warrior_Source_Warrior_WarriorCharacter_h_21_INCLASS_NO_PURE_DECLS \
+	FID_Warrior_Source_Warrior_WarriorCharacter_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> WARRIOR_API UClass* StaticClass<class AWarriorCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Warrior_Source_Warrior_WarriorCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/Warrior/UHT/WarriorGameMode.generated.h
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "WarriorGameMode.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef WARRIOR_WarriorGameMode_generated_h
+#error "WarriorGameMode.generated.h already included, missing '#pragma once' in WarriorGameMode.h"
+#endif
+#define WARRIOR_WarriorGameMode_generated_h
+
+#define FID_Warrior_Source_Warrior_WarriorGameMode_h_12_SPARSE_DATA
+#define FID_Warrior_Source_Warrior_WarriorGameMode_h_12_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Warrior_Source_Warrior_WarriorGameMode_h_12_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Warrior_Source_Warrior_WarriorGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Warrior_Source_Warrior_WarriorGameMode_h_12_ACCESSORS
+#define FID_Warrior_Source_Warrior_WarriorGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAWarriorGameMode(); \
+	friend struct Z_Construct_UClass_AWarriorGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AWarriorGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Warrior"), WARRIOR_API) \
+	DECLARE_SERIALIZER(AWarriorGameMode)
+
+
+#define FID_Warrior_Source_Warrior_WarriorGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	WARRIOR_API AWarriorGameMode(AWarriorGameMode&&); \
+	WARRIOR_API AWarriorGameMode(const AWarriorGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(WARRIOR_API, AWarriorGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AWarriorGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AWarriorGameMode) \
+	WARRIOR_API virtual ~AWarriorGameMode();
+
+
+#define FID_Warrior_Source_Warrior_WarriorGameMode_h_9_PROLOG
+#define FID_Warrior_Source_Warrior_WarriorGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Warrior_Source_Warrior_WarriorGameMode_h_12_SPARSE_DATA \
+	FID_Warrior_Source_Warrior_WarriorGameMode_h_12_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Warrior_Source_Warrior_WarriorGameMode_h_12_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Warrior_Source_Warrior_WarriorGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Warrior_Source_Warrior_WarriorGameMode_h_12_ACCESSORS \
+	FID_Warrior_Source_Warrior_WarriorGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Warrior_Source_Warrior_WarriorGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> WARRIOR_API UClass* StaticClass<class AWarriorGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Warrior_Source_Warrior_WarriorGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
